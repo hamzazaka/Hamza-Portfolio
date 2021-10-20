f66eb700ada5dca9fed9b7cb2454cbb4f63a932b
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-20 12:32:02 -0400

    working on portfolio for the second link

---
+++ portfolio-app/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-dark menu1 ">
+        <a className="navbar-brand logo" href="#"></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+    
+        <div className="collapse navbar-collapse navii" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0 navii">
+                <li className="nav-item ">
+                    <a className="nav-link" href="#">Work<span className="sr-only"></span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">News</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link " href="#">About</a>
+                </li>
+                <li className="nav-item mynav">
+                    <a className="nav-link " href="#">Careers</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link " href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+        
+    )
+}
