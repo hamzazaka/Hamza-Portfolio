73c50230d393eaf3e8a7c98b586a770ea8d94c52
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-20 14:16:14 -0400

    almost on header

--- portfolio-app/src/components/Header.js
+++ portfolio-app/src/components/Header.js
@@ -7,10 +7,23 @@ export default function Header() {
          <div className="contain heading1 ">
         <div className="row">
             <div className="col-md-8 mx-auto hh">
-                <h1>Brand-Led Business Building™</h1>
-                <h1>makes big happen for your</h1>
+                <h1>Hey, I'm <span className='lasth'>{' '}
+      <Typical
+      loop={Infinity}
+      wrapper ='b'
+      steps={[
+          'Hamza Zaka 👋',
+          1000,
+          'a Web Developer ',
+          1000,
+          'a Software Engineer',
+          1000,
+
+      ]}/> </span></h1>
+                <h2>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product
+      </h2>
                 {/* <!-- <h1 className="lasth">company</h1> --> */}
-                <div className='lasth'>
+                {/* <div className='lasth'>
                     <p> {' '}
       <Typical
       loop={Infinity}
@@ -25,7 +38,7 @@ export default function Header() {
 
       ]}/>
       </p>
-    </div>
+    </div> */}
                 <a className="btn btn-danger " href="">The BLBB Effect</a>
             </div>
         </div>
