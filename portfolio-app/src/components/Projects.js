dd7e007a0d89e37f7cea19a8946475e15baf988e
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-23 15:20:28 -0400

    wokring on the projects component

--- portfolio-app/src/components/Projects.js
+++ portfolio-app/src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import pic from '../assets/mobileandlaptop.png';
 
 export default function Projects() {
     return (
@@ -7,8 +8,26 @@ export default function Projects() {
             <h1>Projects</h1>
             <p>Here you will find some of the personal and clients projects that I created with each project containing its own case study</p>
             </div>
+
             <div className="row">
-                
+                <div className="col-md-6 project-img">
+                    <img src={pic} alt="project" />
+                </div>
+                <div className="col-md-6 project-description">
+                    <h1>hello app</h1>
+                    <p>This is a great app </p>
+                    <a href="">button</a>
+                </div>
+            </div>
+             <div className="row">
+                <div className="col-md-6 project-description">
+                    <h1>hello app</h1>
+                    <p>This is a great app </p>
+                    <a href="">button</a>
+                </div>
+                <div className="col-md-6 project-img">
+                    <img src={pic} alt="project" />
+                </div>
             </div>
         </div>
     )
