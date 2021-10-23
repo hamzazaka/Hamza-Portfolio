84845175c444f693fec677908902eedf17436394
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-23 13:16:50 -0400

    working on about mesection

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Aboutme from './components/Aboutme';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Header/>
+      <Aboutme/>
      <h1>hello world</h1>
     </div>
   );
---
+++ portfolio-app/src/components/Aboutme.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Aboutme() {
+    return (
+        <div className='aboutme'>
+            <h1>About me</h1>
+            <p>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
+            <div className="row">
+                <div className="col-md-6">hello</div>
+                <div className="col-md-6">hello</div>
+            </div>
+        </div>
+    )
+}
