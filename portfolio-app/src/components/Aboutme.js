1bcd8dc43e63fd23cdd28e22950149b03cd195f5
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-23 21:48:08 -0400

    making minor changes on it

--- portfolio-app/src/components/Aboutme.js
+++ portfolio-app/src/components/Aboutme.js
@@ -13,7 +13,7 @@ export default function Aboutme() {
                     <h2>Get to know me</h2>
                     <p>I'm a <strong>Frontend Web Developer</strong> building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the <strong>Projects</strong> section.</p>
                     <p>I also like sharing content related to the stuff that I have learned over the years in
-                     <strong> Web Development</strong> so it can help other people of the Dev Community. Feel free to Connect or Follow me on my <a className='p-linkedin' href="">Linkedin</a> where I post useful content related to Web Development and Programming</p>
+                     <strong> Web Development</strong> so it can help other people of the Dev Community. Feel free to Connect or Follow me on my <a className='p-linkedin' target='_blank' href="https://www.linkedin.com/in/hamzazaka/">Linkedin</a> where I post useful content related to Web Development and Programming</p>
                     <p>I'm open to <strong>Job</strong> opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to <strong>contact </strong> me.</p>
                     <Link smooth={true} to='Contact' offset={-110} className='btn btn-contact' href=""><span></span> Contact</Link>
 
