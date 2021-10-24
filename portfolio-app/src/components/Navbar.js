5db17a2eef8b553490db3e01bbd5240257d13459
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-24 02:00:38 -0400

    making minor changes on navbar

--- portfolio-app/src/components/Navbar.js
+++ portfolio-app/src/components/Navbar.js
@@ -28,6 +28,7 @@ export default function Navbar() {
                 <li className="nav-item">
                     <a className="nav-link link2 " target='_blank' href="https://drive.google.com/file/d/1TC-h-Vmehb_pEV1bFjP6HPCkp8ShbxLl/view"> Resume</a>
                 </li>
+                {/* <a className='btn btn-dang' target='_blank' href="https://drive.google.com/file/d/1TC-h-Vmehb_pEV1bFjP6HPCkp8ShbxLl/view">RESUME</a> */}
             </ul>
         </div>
     </nav>
