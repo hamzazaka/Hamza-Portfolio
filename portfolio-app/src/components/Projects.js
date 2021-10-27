1e5ebc2fa7de5fc32d24ee6b6994565f3a6c5898
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-27 18:10:36 -0400

    adding the images and the project links

--- portfolio-app/src/components/Projects.js
+++ portfolio-app/src/components/Projects.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import pic from '../assets/mobileandlaptop.png';
-import covid from '../assets/covid19.png';
+import covid19 from '../assets/covid-19.png';
+import bookstop from '../assets/bookstop-react.png';
+import dashboardreact from '../assets/dashboard-react.png';
+import portfolio from '../assets/hamza-portfolio.png';
+// import pic2 from '../assets/project.png';
 
 export default function Projects() {
     return (
@@ -12,7 +15,7 @@ export default function Projects() {
 
             <div className="row">
                 <div className="col-md-6 project-img">
-                    <img src={covid} alt="project" />
+                    <img src={covid19} alt="project" />
                 </div>
                 <div className="col-md-6 project-description">
                     <h1>Covid-19 Tracker</h1>
@@ -27,7 +30,33 @@ export default function Projects() {
 
                <div className="row">
                 <div className="col-md-6 project-img">
-                    <img src={covid} alt="project" />
+                    <img src={dashboardreact} alt="project" />
+                </div>
+                <div className="col-md-6 project-description pd-2">
+                    <h1>Hamza Porfolio</h1>
+                    <p>Yubter is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <a href="" className='btn btn-contact'>Github</a>
+                    <a href="" className='btn btn-contact'>Live </a>
+                </div>
+
+            </div>
+
+            <div className="row">
+                <div className="col-md-6 project-img">
+                    <img src={portfolio} alt="project" />
+                </div>
+                <div className="col-md-6 project-description pd-2">
+                    <h1>Hamza Porfolio</h1>
+                    <p>Yubter is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <a href="" className='btn btn-contact'>Github</a>
+                    <a href="" className='btn btn-contact'>Live </a>
+                </div>
+
+            </div>
+
+            <div className="row">
+                <div className="col-md-6 project-img">
+                    <img src={bookstop} alt="project" />
                 </div>
                 <div className="col-md-6 project-description pd-2">
                     <h1>Hamza Porfolio</h1>
