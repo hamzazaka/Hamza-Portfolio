845cdbdb0111f9c7f0a53d5cf71921992b02e92a
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-10-27 22:01:23 -0400

    fiished uploadng the links in the applicaiton

--- portfolio-app/src/components/Projects.js
+++ portfolio-app/src/components/Projects.js
@@ -20,7 +20,7 @@ export default function Projects() {
                 <div className="col-md-6 project-description">
                     <h1>Covid-19 Tracker</h1>
                     <p>Covid19 is a <strong>React-Js project</strong> developed in support of people suffering and affected due to Covid-19 virus all around the world. The app takes a very simple and light weight design keeping in mind proper responsiveness throughout different platforms like web and mobile responsiveness. The app lets the user track the growth of active cases in their country and provides statistics from all around the world.</p>
-                     <p><strong>Technologies :</strong> React, Chart.js, React-Hooks, JavaScript, CSS, Bootstrap, Material UI</p>
+                     <p><strong>Technologies :</strong> React, Chart.js, React-Hooks, JavaScript, CSS, Bootstrap, Material UI, JSON, Rest Apis</p>
                     <a target='_blank' href="https://github.com/hamzazaka/COVID-19-Tracker" className='btn btn-contact'>Github</a>
                     <a target='_blank' href="https://covid-19-my.netlify.app" className='btn btn-contact'>Live </a>
                 </div>
@@ -35,7 +35,7 @@ export default function Projects() {
                 <div className="col-md-6 project-description pd-2">
                     <h1>Admin DashBoard</h1>
                     <p>Admin React Dashboard coded in <strong>React</strong>. It comes packed with a set of beautiful elements and components built by just Bootstrap, React and Chart.js. It has a fully responsive layout, multiple color schemes for both Bootstrap elements and layout components. Admin React Dashboard is powered by Bootstrap 4, Webpack and it uses create-react-app.</p>
-                     <p><strong>Technologies :</strong> React, Chart.js, React-Hooks, JavaScript, CSS, Bootstrap, Material UI</p>
+                     <p><strong>Technologies :</strong> React, Chart.js, React-Hooks, JavaScript, CSS, Bootstrap, Material UI, JSON, Rest Apis, Yarn</p>
                     <a href="https://github.com/hamzazaka/react-dashboard" className='btn btn-contact'>Github</a>
                     <a href="https://admin-dashboardbyreact.netlify.app/" className='btn btn-contact'>Live </a>
                 </div>
@@ -48,9 +48,10 @@ export default function Projects() {
                 </div>
                 <div className="col-md-6 project-description pd-2">
                     <h1>Hamza Porfolio</h1>
-                    <p>Yubter is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <a href="" className='btn btn-contact'>Github</a>
-                    <a href="" className='btn btn-contact'>Live </a>
+                    <p>My Portfolio which you are looking at right now is build by using <strong>React.js</strong>. It has fully responsive layout built by using latest version of Bootstrap both compatible with mobile and web. The single page application using different technologies to making it fully responsive and impress the recruiters. </p>
+                     <p><strong>Technologies :</strong> React, React-Hooks, JavaScript, CSS, Bootstrap, NPM packages</p>
+                    <a href="https://github.com/hamzazaka/Hamza-Portfolio" className='btn btn-contact'>Github</a>
+                    <a href="https://hamzaportfolio.netlify.app/" className='btn btn-contact'>Live </a>
                 </div>
 
             </div>
@@ -61,7 +62,8 @@ export default function Projects() {
                 </div>
                 <div className="col-md-6 project-description pd-2">
                     <h1>Hamza Porfolio</h1>
-                    <p>Yubter is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>BookStop is a React app that works with the BookStop API server-side. The app allows a user to log in, track books they have already read and books they want to read in two separate lists, and search for new reads using the Google API.</p>
+                     <p><strong>Technologies :</strong> React, React-Hooks, JavaScript, CSS, Bootstrap, Node.js, MERN, Yarn, Canva </p>
                     <a href="" className='btn btn-contact'>Github</a>
                     <a href="" className='btn btn-contact'>Live </a>
                 </div>
