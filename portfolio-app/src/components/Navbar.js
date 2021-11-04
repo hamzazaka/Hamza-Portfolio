c63c5d49f9889e8208f8b7abdf89f98d839f2167
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-04 13:29:40 -0400

    working on some cahgnes regarding firebase

--- portfolio-app/src/components/Navbar.js
+++ portfolio-app/src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
         <div className="collapse navbar-collapse navii" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto mt-2 mt-lg-0 navii">
                 <li className="nav-item ">
-                    <Link smooth={true} to='home' offset={-110} className="nav-link" href="#"> <i class="fas fa-user"></i> Home<span className="sr-only"></span></Link>
+                    <Link smooth={true} to='home' offset={-110} className="nav-link" href="#"> <i class="fas fa-user"></i> Hamza<span className="sr-only"></span></Link>
                 </li>
                 <li className="nav-item">
                     <Link smooth={true} to='About' offset={-110}  className="nav-link" href="#"><i class="fas fa-laptop-code"></i> About</Link>
