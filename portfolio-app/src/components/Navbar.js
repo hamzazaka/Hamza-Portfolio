48c042cb5ff8bacb3c64ad613e46a48d61a0898e
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-10 17:14:24 -0500

    adding logs

--- portfolio-app/src/components/Navbar.js
+++ portfolio-app/src/components/Navbar.js
@@ -29,6 +29,9 @@ export default function Navbar() {
                 <li className="nav-item">
                     <a rel="noreferrer noopener" className="nav-link link2 " target='_blank' href={resume}> Resume</a>
                 </li>
+                <li className="nav-item">
+                    <a rel="noreferrer noopener" className="nav-link link2 " target='_blank' href='https://hamzazaka.medium.com/'> My Tech Blogs</a>
+                </li>
                 {/* <a className='btn btn-dang' target='_blank' href="https://drive.google.com/file/d/1TC-h-Vmehb_pEV1bFjP6HPCkp8ShbxLl/view">RESUME</a> */}
             </ul>
         </div>
